ca4806729536e8a304836d60dfe19bc3800b35b2
Brad Reid <brad.reid@canadadrugs.com>
2011-11-19 13:21:17 -0600

    update notifier

--- app/models/contact_request.rb
+++ app/models/contact_request.rb
@@ -2,10 +2,8 @@ class ContactRequest < ActiveRecord::Base
   validates_presence_of :name, :email, :subject, :message
   after_create :notify
   
-private
-
   def notify
-    ContactRequestEmail.all do |c|
+    ContactRequestEmail.all.each do |c|
       Notifier.contact_request(self, c).deliver
     end
   end
