89af1335a1340945ebe15c557e707f8b84125718
Brad Reid <brad.reid@canadadrugs.com>
2012-02-06 12:41:51 -0600

    update images

--- app/models/album_picture.rb
+++ app/models/album_picture.rb
@@ -2,7 +2,7 @@ class AlbumPicture < ActiveRecord::Base
     
   belongs_to :album
   
-  has_attached_file :image, :styles => { :medium => "250x250>"},:storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => "/:style/:filename"  
+  has_attached_file :image, :styles => { :medium => "250x250>"},:storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => "/:style/:id:filename"  
   
   validates_attachment_presence :image
   
