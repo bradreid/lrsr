ecf43ed8e321351e5a886d207feb26473198e784
Brad Reid <brad.reid@canadadrugs.com>
2012-03-07 17:14:26 -0600

    don't allow registrations

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,17 @@
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    raise "don't do it"
+  end
+
+  def create
+    raise "don't do it"
+  end
+
+  def update
+    raise "don't do it"
+  end
+  
+  def edit
+    raise "don't do it"
+  end
+end
