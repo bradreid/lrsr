e4d20bae817bcd5f27feb6df6fc845645921850a
Brad Reid <brad.reid@canadadrugs.com>
2012-03-18 18:05:34 -0500

    go back to standard ar

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -7,15 +7,6 @@ class ActiveRecord::Base
   def current_user
     Thread.current["current_thread_user"] || User.system_user
   end
-  
-  def update_attribute_without_timestamps(attribute, value)
-    self.record_timestamps = false 
-    begin
-      self.update_attribute(attribute,value)
-    ensure
-       self.record_timestamps = true 
-    end
-  end  
     
   class << self
     def current_user
