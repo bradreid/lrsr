b37a652c70efad08919cdff6e97531c0344e2f88
Brad Reid <brad.reid@canadadrugs.com>
2012-03-18 17:53:02 -0500

    add without timestamps

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -21,5 +21,14 @@ class ActiveRecord::Base
     def current_user
       Thread.current["current_thread_user"] || User.system_user
     end
+    
+    def without_timestamps(*models, &block)
+      models.each{|m| m.record_timestamps = false }
+      begin
+        yield
+      ensure
+        models.each{|m| m.record_timestamps = true }
+      end
+    end    
   end
 end
