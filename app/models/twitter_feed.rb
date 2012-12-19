5755a4ff6d19e392605dce3b042c3a604b1afced
Brad Reid <bradreid20@gmail.com>
2012-12-19 12:24:17 -0600

    don't send tweets in dev mode'

--- app/models/twitter_feed.rb
+++ app/models/twitter_feed.rb
@@ -1,5 +1,5 @@
 class TwitterFeed < ActiveRecord::Base
-  
+
   def self.create_feed
     n = nil
     begin
@@ -9,7 +9,7 @@ class TwitterFeed < ActiveRecord::Base
     self.connection.execute("delete from twitter_feeds where id <> #{n.id}") if n
     n
   end
-  
+
   def self.next_tweet
     events_today = Event.all(:conditions => ["shared_before is NULL and start <= ? AND updated_at < ?", Time.now + 1.day, Time.now - 30.minutes])
     events_today.first.update_attribute(:shared_before, Time.now) if events_today.any?
@@ -19,15 +19,16 @@ class TwitterFeed < ActiveRecord::Base
     new_trail_conditions = TrailCondition.all(:conditions => ["shared is null and user_updated_at < ?", Time.now - 1.minutes], :order => 'id asc')
     tweets = events_today + new_trail_conditions + new_articles + new_events + new_newsletters
     tweets.first
-  end  
-  
+  end
+
   def self.send_next_tweet
+    return unless Rails.env.production?
     nt = self.next_tweet
     return unless nt
     nt.update_attribute(:shared,Time.now)
     Tweet.new('lrsrclub').tweet(nt.tweet)
   end
-  
+
   def self.new_if_needed
     last = TwitterFeed.last
     if last && (last.created_at.utc > 30.minutes.ago.utc)
@@ -35,6 +36,6 @@ class TwitterFeed < ActiveRecord::Base
     else
       self.send_next_tweet
       self.create_feed
-    end    
+    end
   end
 end
